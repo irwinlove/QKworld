0ff98c31cf5e83737dbaa71b51cfbb4769284eb2
irwin <84185456@qq.com>
2015-09-14 18:02:05 +0800

    add models

---
+++ blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,render_to_response
+
+# Create your views here.
+from blog.models import Artical,Author,Tag,Classification
+from django.template import RequestContext
